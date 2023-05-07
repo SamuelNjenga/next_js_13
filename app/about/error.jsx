b354e0a499e2ba4f201d1d4d93e7b2b091b2e7ac
Samuel Njenga <snjenga704@gmail.com>
2023-05-07 12:39:22 +0300

    feat: add error.jsx in about page

---
+++ app/about/error.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Error({ error, reset }) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <h2>{error.message}</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const About = () => {
+    throw new Error("failed to load data")
   return (
     <div>
       <h1>About page</h1>
