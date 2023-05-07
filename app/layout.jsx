3e25a98984fc911bc6f24b27239af7a1e5035ae3
Samuel Njenga <snjenga704@gmail.com>
2023-05-07 12:01:46 +0300

    feat: add nav component to RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <h1>Logo</h1>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
