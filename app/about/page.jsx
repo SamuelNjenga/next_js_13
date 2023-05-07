51a3887cd1d97cd6ef00bb154386ac18e6593cfa
Samuel Njenga <snjenga704@gmail.com>
2023-05-07 12:46:12 +0300

    feat: add Loading component to about directory

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-    throw new Error("failed to load data")
+    // throw new Error("failed to load data")
   return (
     <div>
       <h1>About page</h1>
