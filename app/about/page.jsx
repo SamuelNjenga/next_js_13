78d0aeff102a270cdd8984b2c24c1c42063f43b1
Samuel Njenga <snjenga704@gmail.com>
2023-05-07 20:20:55 +0300

    style: add style to About page header

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const About = () => {
-    // throw new Error("failed to load data")
+  // throw new Error("failed to load data")
   return (
     <div>
-      <h1>About page</h1>
+      <h1 className="text-lg py-2 m-4">About page</h1>
     </div>
   );
 };
