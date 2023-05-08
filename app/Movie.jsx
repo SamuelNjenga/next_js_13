7f725191e26c9d24ec68ceeae8418627cc593641
Samuel Njenga <snjenga704@gmail.com>
2023-05-08 12:42:08 +0300

    feat: add generateStaticParams function

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -6,7 +6,7 @@ export default function Movie({ title, id, poster_path, release_date }) {
     <div>
       <h1>{title}</h1>
       <h2>{release_date}</h2>
-      <Link href={`/asd`}>
+      <Link href={`/${id}`}>
         <Image
           src={imagePath + poster_path}
           width={800}
---
+++ app/[movie]/page.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export async function generateStaticParams() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  return res.results.map((movie) => ({ movie: toString(movie.id) }));
+}
+
+export default async function MovieDetail({ params }) {
+  const { movie } = params;
+  const imagePath = "https://image.tmdb.org/t/p/original";
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`,
+    { next: { revalidate: 0 } }
+  );
+  const res = await data.json();
+  return (
+    <div>
+      <div>
+        <h2 className="text-2xl">{res.title}</h2>
+        <h2 className="text-lg">{res.release_date}</h2>
+        <h2>Runtime: {res.runtime} minutes</h2>
+        <h2 className="text-sm bg-green-600 inline-block my-2 py-2 px-4 rounded-md">
+          {res.status}
+        </h2>
+        <Image
+          className="my-12 w-full"
+          src={imagePath + res.backdrop_path}
+          width={1000}
+          height={1000}
+          priority
+          alt={res.title}
+        />
+        <p>{res.overview}</p>
+      </div>
+    </div>
+  );
+}
