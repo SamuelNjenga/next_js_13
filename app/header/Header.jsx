2118a53534f5b1a313b4e6fb80ab3242b86441c1
Samuel Njenga <snjenga704@gmail.com>
2023-05-15 20:30:15 +0300

    feat: add Link component to Header file

--- app/header/Header.jsx
+++ app/header/Header.jsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <header className="p-5 bg-blue-500">
-  <p className="font-bold text-white">I am a header</p>
-  </header>;
+  return (
+    <header className="p-5 bg-blue-500">
+      <Link href="/" className="px-2 py-1 bg-white text-blue-500 rounded-lg">
+        Home
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
