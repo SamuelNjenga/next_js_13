5a3118329f5327f533b6f5bf904c5283eb98b525
Samuel Njenga <snjenga704@gmail.com>
2023-05-16 22:00:14 +0300

    feat: add not-found.jsx

---
+++ app/todos/[todoId]/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            Whoops we couldn't find the TODO you are looking for
+        </div>
+    )
+}
+
+export default NotFound
--- app/todos/[todoId]/page.jsx
+++ app/todos/[todoId]/page.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
-const TodoPage = ({ params: { todoId } }) => {
-  return <div>TodoPage: {todoId}</div>;
+import { notFound } from "next/navigation";
+export const dynamicParams = true;
+const fetchTodo = async (todoId) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  );
+  const todo = await res.json();
+  console.log("todos", todo);
+  return todo;
 };
 
-export default TodoPage;
+export default async function TodoPage({ params: { todoId } }) {
+  const todo = await fetchTodo(todoId);
+  if (!todo.id) return notFound();
+  return (
+    <div className="p-10 bg-yellow-200 bottom-2 m-2 shadow-lg">
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+      <p className="border-t border-black mt-5 text-right">
+        By User: {todo.userId}
+      </p>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
+  const todos = await res.json();
+  console.log("todos", todos);
+
+  // For this expo only, we are prebuilding the first 10 pages to avoid being rate limited by the DEMO API
+  const trimmedTodos = todos.splice(0, 10);
+  return trimmedTodos.map((todo) => ({ todo: toString(todo.id) }));
+}
